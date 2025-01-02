b571385248998b10cef814a8bc764d5f4ebbd8fb
YASSINE-AA <overflowtn@gmail.com>
2025-01-03 00:23:45 +0100

    added multi-window support

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 LIBS = -lGL -lEGL -lm -lX11  -lwayland-client -lwayland-server -lwayland-cursor -lwayland-egl
-CFLAGS = -I/usr/include/cairo -I/usr/include/glib-2.0 -I/usr/lib/glib-2.0/include -I/usr/include/pixman-1 -I/usr/include/freetype2 -I/usr/include/libdrm -I/usr/include/libpng12  -I/usr/include
+CFLAGS =  -Wall -Wextra -Wno-unused-variable -Wno-unused-parameter -g3 -fsanitize=address,undefined -I/usr/include/cairo -I/usr/include/glib-2.0 -I/usr/lib/glib-2.0/include -I/usr/include/pixman-1 -I/usr/include/freetype2 -I/usr/include/libdrm -I/usr/include/libpng12  -I/usr/include
 
 test : 
 	gcc xdg-shell.c wayland_utils.c xdg-decorations.c glad.c wayland_backend.c ${CFLAGS} -o build/test ${LIBS}
--- wayland_backend.c
+++ wayland_backend.c
@@ -5,11 +5,16 @@
 #include <stdbool.h>
 #include <string.h>
 #include <sys/mman.h>
+#include <assert.h>
 #include <time.h>
 #include <unistd.h>
 #include <wayland-client.h>
+
+// XDG HEADERS
 #include "xdg-shell.h"
 #include "xdg-decorations.h"
+#include "xdg-dialog.h"
+
 #include "wayland_utils.h"
 #include <wayland-egl.h>
 #include <EGL/egl.h>
@@ -19,7 +24,8 @@
 #include <stdlib.h>
 #include <assert.h>
 
-typedef struct {
+typedef struct
+{
     GLuint textureID;
     int width;
     int height;
@@ -34,16 +40,16 @@ struct GooeyBackendContext
     struct wl_registry *wl_registry;
     struct wl_compositor *wl_compositor;
     struct xdg_wm_base *xdg_wm_base;
-    struct wl_surface *wl_surface;
-    struct xdg_surface *xdg_surface;
-    struct xdg_toplevel *xdg_toplevel;
+    struct wl_surface **wl_surfaces;
+    struct xdg_surface **xdg_surfaces;
+    struct xdg_toplevel **xdg_toplevels;
     struct zxdg_decoration_manager_v1 *decoration_manager;
 
     GLuint *text_programs;
     GLuint shape_program;
     GLuint text_vbo;
     GLuint shape_vbo;
-    //userPtr *user_ptrs;
+    // userPtr *user_ptrs;
     GLuint *text_vaos;
     GLuint *shape_vaos;
     mat4x4 projection;
@@ -51,8 +57,8 @@ struct GooeyBackendContext
     GLuint text_vertex_shader;
     Character characters[128];
     char font_path[256];
-    int window_count;
-    bool inhibit_reset; 
+    size_t window_count;
+    bool inhibit_reset;
     unsigned int selected_color;
 
     struct
@@ -60,9 +66,9 @@ struct GooeyBackendContext
         EGLDisplay dpy;
         EGLContext ctx;
         EGLConfig conf;
-        EGLSurface surface;
+        EGLSurface *surfaces;
+        struct wl_egl_window **windows;
     } egl;
-
 };
 
 static struct GooeyBackendContext ctx = {0};
@@ -75,7 +81,7 @@ void check_shader_compile(GLuint shader)
     {
         char infoLog[512];
         glGetShaderInfoLog(shader, 512, NULL, infoLog);
-       // LOG_ERROR("ERROR: Shader compilation failed\n%s\n", infoLog);
+        // LOG_ERROR("ERROR: Shader compilation failed\n%s\n", infoLog);
         exit(EXIT_FAILURE);
     }
 }
@@ -88,7 +94,7 @@ void check_shader_link(GLuint program)
     {
         char infoLog[512];
         glGetProgramInfoLog(program, 512, NULL, infoLog);
-      //  LOG_ERROR("ERROR: Program linking failed\n%s\n", infoLog);
+        //  LOG_ERROR("ERROR: Program linking failed\n%s\n", infoLog);
         exit(EXIT_FAILURE);
     }
 }
@@ -112,6 +118,9 @@ static void handle_global(void *data, struct wl_registry *registry, uint32_t id,
                                                  &zxdg_decoration_manager_v1_interface,
                                                  version);
     }
+    else if (strcmp(interface, "xdg_wm_dialog_v1") == 0)
+    {
+    }
 }
 
 static void handle_global_remove(void *data, struct wl_registry *registry, uint32_t name) {}
@@ -120,35 +129,8 @@ static const struct wl_registry_listener registry_listener = {
     .global = handle_global,
     .global_remove = handle_global_remove,
 };
-
-static void wayland_create_xdg_surface(void)
-{
-    ctx.xdg_surface = xdg_wm_base_get_xdg_surface(ctx.xdg_wm_base, ctx.wl_surface);
-    assert(ctx.xdg_surface);
-
-    ctx.xdg_toplevel = xdg_surface_get_toplevel(ctx.xdg_surface);
-    assert(ctx.xdg_toplevel);
-
-    xdg_toplevel_set_title(ctx.xdg_toplevel, "Wayland Desktop OpenGL Example");
-
-    if (ctx.decoration_manager)
-    {
-
-        struct zxdg_toplevel_decoration_v1 *decoration =
-            zxdg_decoration_manager_v1_get_toplevel_decoration(ctx.decoration_manager, ctx.xdg_toplevel);
-        zxdg_toplevel_decoration_v1_set_mode(decoration, ZXDG_TOPLEVEL_DECORATION_V1_MODE_SERVER_SIDE);
-    }
-
-    wl_surface_commit(ctx.wl_surface);
-}
-
 static void wayland_init_egl(void)
 {
-    static const EGLint context_attribs[] = {
-        EGL_CONTEXT_MAJOR_VERSION, 4,
-        EGL_CONTEXT_MINOR_VERSION, 5,
-        EGL_CONTEXT_OPENGL_PROFILE_MASK_KHR, EGL_CONTEXT_OPENGL_CORE_PROFILE_BIT_KHR,
-        EGL_NONE};
 
     EGLint config_attribs[] = {
         EGL_SURFACE_TYPE, EGL_WINDOW_BIT,
@@ -183,66 +165,45 @@ static void wayland_init_egl(void)
         fprintf(stderr, "Failed to bind OpenGL API\n");
         exit(EXIT_FAILURE);
     }
-
-    ctx.egl.ctx = eglCreateContext(ctx.egl.dpy, ctx.egl.conf, EGL_NO_CONTEXT, context_attribs);
-    if (ctx.egl.ctx == EGL_NO_CONTEXT)
-    {
-        fprintf(stderr, "Failed to create EGL context\n");
-        exit(EXIT_FAILURE);
-    }
-
-    struct wl_egl_window *egl_window = wl_egl_window_create(ctx.wl_surface, 640, 480);
-    assert(egl_window);
-
-    ctx.egl.surface = eglCreateWindowSurface(ctx.egl.dpy, ctx.egl.conf, egl_window, NULL);
-    if (ctx.egl.surface == EGL_NO_SURFACE)
-    {
-        fprintf(stderr, "Failed to create EGL surface\n");
-        exit(EXIT_FAILURE);
-    }
-
-    if (!eglMakeCurrent(ctx.egl.dpy, ctx.egl.surface, ctx.egl.surface, ctx.egl.ctx))
-    {
-        fprintf(stderr, "Failed to make EGL context current\n");
-        exit(EXIT_FAILURE);
-    }
-
-    printf("EGL context and surface created successfully\n");
 }
 
 static void wayland_cleanup_egl(void)
 {
-    eglDestroySurface(ctx.egl.dpy, ctx.egl.surface);
+    for (size_t i = 0; i < ctx.window_count; ++i)
+        eglDestroySurface(ctx.egl.dpy, ctx.egl.surfaces[i]);
     eglDestroyContext(ctx.egl.dpy, ctx.egl.ctx);
     eglTerminate(ctx.egl.dpy);
 }
 
-static void wayland_cleanup_gl(void) {
+static void wayland_cleanup_gl(void)
+{
 
-    if(ctx.text_vaos) {
-      free(ctx.text_vaos);
-      ctx.text_vaos = NULL;
+    if (ctx.text_vaos)
+    {
+        free(ctx.text_vaos);
+        ctx.text_vaos = NULL;
     }
 
-    if(ctx.shape_vaos) {
-      free(ctx.shape_vaos);
-      ctx.shape_vaos = NULL;
+    if (ctx.shape_vaos)
+    {
+        free(ctx.shape_vaos);
+        ctx.shape_vaos = NULL;
     }
 
-    if(ctx.text_programs) {
-       free(ctx.text_programs);
-       ctx.text_programs = NULL;
+    if (ctx.text_programs)
+    {
+        free(ctx.text_programs);
+        ctx.text_programs = NULL;
     }
 }
-
 
 void wayland_setup_shared()
 {
 
     glGenBuffers(1, &ctx.text_vbo);
-
     ctx.text_vertex_shader = glCreateShader(GL_VERTEX_SHADER);
     set_shader_src_file("shaders/text/text_vertex.glsl", ctx.text_vertex_shader);
+
     glCompileShader(ctx.text_vertex_shader);
     check_shader_compile(ctx.text_vertex_shader);
 
@@ -309,14 +270,8 @@ void wayland_setup_seperate_vao(int window_id)
     ctx.shape_vaos[window_id] = shape_vao;
 }
 
-
 static void wayland_init_gl(void)
 {
-    ctx.text_vaos = (GLuint *)malloc(sizeof(GLuint) * 100);
-    ctx.shape_vaos = (GLuint *)malloc(sizeof(GLuint) * 100);
-    //ctx.user_ptrs = (userPtr *)malloc(sizeof(userPtr) * 100);
-    ctx.text_programs = (GLuint *)malloc(sizeof(GLuint) * 100);
-
     if (!gladLoadGLLoader((GLADloadproc)eglGetProcAddress))
     {
         fprintf(stderr, "Failed to initialize GLAD\n");
@@ -324,14 +279,14 @@ static void wayland_init_gl(void)
     }
 }
 
-static void draw_frame(void)
+static void draw_frame(int window_id)
 {
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
     glUseProgram(ctx.shape_program);
 
-    glBindVertexArray(ctx.shape_vaos[0]);
+    glBindVertexArray(ctx.shape_vaos[window_id]);
 
     Vertex vertices[] = {
         {{-0.5f, -0.5f}, {1.0f, 0.0f, 0.0f}},
@@ -339,8 +294,7 @@ static void draw_frame(void)
         {{0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}},
         {{-0.5f, -0.5f}, {1.0f, 0.0f, 0.0f}},
         {{0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}},
-        {{-0.5f, 0.5f}, {1.0f, 1.0f, 0.0f}}
-    };
+        {{-0.5f, 0.5f}, {1.0f, 1.0f, 0.0f}}};
 
     glBindBuffer(GL_ARRAY_BUFFER, ctx.shape_vbo);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_DYNAMIC_DRAW);
@@ -353,6 +307,17 @@ static void draw_frame(void)
 
 static void wayland_setup(void)
 {
+
+    ctx.text_vaos = (GLuint *)malloc(sizeof(GLuint) * 100);
+    ctx.shape_vaos = (GLuint *)malloc(sizeof(GLuint) * 100);
+    // ctx.user_ptrs = (userPtr *)malloc(sizeof(userPtr) * 100);
+    ctx.text_programs = (GLuint *)malloc(sizeof(GLuint) * 100);
+    ctx.wl_surfaces = (struct wl_surface **)malloc(sizeof(struct wl_surface *) * 100);
+    ctx.xdg_surfaces = (struct xdg_surface **)malloc(sizeof(struct xdg_surface *) * 100);
+    ctx.xdg_toplevels = (struct xdg_toplevel **)malloc(sizeof(struct xdg_toplevel *) * 100);
+    ctx.egl.surfaces = (EGLSurface *)malloc(sizeof(EGLSurface) * 100);
+    ctx.egl.windows = (struct wl_egl_window **)malloc(sizeof(struct wl_egl_window *) * 100);
+    ctx.window_count = 0;
 
     ctx.wl_display = wl_display_connect(NULL);
     assert(ctx.wl_display);
@@ -370,16 +335,101 @@ static void wayland_setup(void)
         fprintf(stderr, "Failed to retrieve Wayland compositor or xdg_wm_base\n");
         exit(EXIT_FAILURE);
     }
-
-    ctx.wl_surface = wl_compositor_create_surface(ctx.wl_compositor);
-    assert(ctx.wl_surface);
-
-    wayland_create_xdg_surface();
 }
 
-static void wayland_disconnect_display()
+static void wayland_disconnect_display(void)
 {
     wl_display_disconnect(ctx.wl_display);
+}
+
+static void wayland_create_ctx(void)
+{
+    static const EGLint context_attribs[] = {
+        EGL_CONTEXT_MAJOR_VERSION, 4,
+        EGL_CONTEXT_MINOR_VERSION, 5,
+        EGL_CONTEXT_OPENGL_PROFILE_MASK_KHR, EGL_CONTEXT_OPENGL_CORE_PROFILE_BIT_KHR,
+        EGL_NONE};
+
+    ctx.egl.ctx = eglCreateContext(ctx.egl.dpy, ctx.egl.conf, EGL_NO_CONTEXT, context_attribs);
+    if (ctx.egl.ctx == EGL_NO_CONTEXT)
+    {
+        fprintf(stderr, "Failed to create EGL context\n");
+        exit(EXIT_FAILURE);
+    }
+}
+
+static void wayland_make_ctx_current(int window_id)
+{
+    if (!eglMakeCurrent(ctx.egl.dpy, ctx.egl.surfaces[window_id], ctx.egl.surfaces[window_id], ctx.egl.ctx))
+    {
+        fprintf(stderr, "Failed to make EGL context current\n");
+        exit(EXIT_FAILURE);
+    }
+}
+
+static void wayland_create_window(void)
+{
+
+    ctx.wl_surfaces[ctx.window_count] = wl_compositor_create_surface(ctx.wl_compositor);
+    assert(ctx.wl_surfaces[ctx.window_count]);
+
+    ctx.xdg_surfaces[ctx.window_count] = xdg_wm_base_get_xdg_surface(ctx.xdg_wm_base, ctx.wl_surfaces[ctx.window_count]);
+    assert(ctx.xdg_surfaces[ctx.window_count]);
+
+    ctx.xdg_toplevels[ctx.window_count] = xdg_surface_get_toplevel(ctx.xdg_surfaces[ctx.window_count]);
+    assert(ctx.xdg_toplevels[ctx.window_count]);
+
+    xdg_toplevel_set_title(ctx.xdg_toplevels[ctx.window_count], "Wayland Desktop OpenGL Example");
+
+    /*
+        if (ctx.decoration_manager)
+        {
+
+            struct zxdg_toplevel_decoration_v1 *decoration =
+                zxdg_decoration_manager_v1_get_toplevel_decoration(ctx.decoration_manager, ctx.xdg_toplevel);
+            zxdg_toplevel_decoration_v1_set_mode(decoration, ZXDG_TOPLEVEL_DECORATION_V1_MODE_SERVER_SIDE);
+        }
+
+    */
+    ctx.egl.windows[ctx.window_count] = wl_egl_window_create(ctx.wl_surfaces[ctx.window_count], 640, 480);
+    assert(ctx.egl.windows[ctx.window_count]);
+
+    ctx.egl.surfaces[ctx.window_count] = eglCreateWindowSurface(ctx.egl.dpy, ctx.egl.conf, ctx.egl.windows[ctx.window_count], NULL);
+    if (ctx.egl.surfaces[ctx.window_count] == EGL_NO_SURFACE)
+    {
+        fprintf(stderr, "Failed to create EGL surface\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (ctx.window_count == 0)
+    {
+        // Create one context for all windows.
+        // First window created is the parent.
+        // Other windows created later are all children of the parent window.
+        wayland_create_ctx();
+        wayland_make_ctx_current(0);
+        wayland_init_gl();
+        wayland_setup_shared();
+    }
+
+    wl_surface_commit(ctx.wl_surfaces[ctx.window_count]);
+
+    wayland_setup_seperate_vao(ctx.window_count);
+
+    ctx.window_count++;
+}
+
+static void wayland_render()
+{
+    while (wl_display_dispatch_pending(ctx.wl_display) != -1)
+    {
+        for (size_t window_id = 0; window_id < ctx.window_count; ++window_id)
+        {
+            wayland_make_ctx_current(window_id);
+            draw_frame(window_id);
+            eglSwapBuffers(ctx.egl.dpy, ctx.egl.surfaces[window_id]);
+        }
+    }
 }
 
 int main(int argc, char *argv[])
@@ -387,18 +437,11 @@ int main(int argc, char *argv[])
 
     wayland_setup();
     wayland_init_egl();
-    wayland_init_gl();
-    wayland_setup_shared();
-    wayland_setup_seperate_vao(0); // 0 window id for now (only working on parent)
-
-    while (wl_display_dispatch(ctx.wl_display) != -1)
-    {
-        draw_frame();
-        eglSwapBuffers(ctx.egl.dpy, ctx.egl.surface);
-    }
-
+    wayland_create_window();
+    wayland_create_window();
+    wayland_render();
     wayland_cleanup_egl();
     wayland_cleanup_gl();
-
+    wayland_disconnect_display();
     return 0;
 }
